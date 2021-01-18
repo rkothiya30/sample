4cea35ba7e704462774da46ca594c82ce33c0e3a
Rakesh <rkothiya30@gmail.com>
2021-01-18 13:53:52 +0530

    skipped add

--- src/demo/B.java
+++ src/demo/B.java
@@ -4,9 +4,6 @@ public class B {
 
 	public static void main(String[] args) {
 		System.out.println("this is B file.");
-		System.out.println("this is B file.");
-		System.out.println("this is B file.");
-		System.out.println("this is B file.");
-		System.out.println("this is B file.");
+		System.out.println("next print");
 	}
 }
