6b156e69b078e0170fcefe265669613623c0d406
Rakesh <rkothiya30@gmail.com>
2021-01-18 14:13:47 +0530

    nothing

--- src/demo/B.java
+++ src/demo/B.java
@@ -4,6 +4,6 @@ public class B {
 
 	public static void main(String[] args) {
 		System.out.println("this is B file.");
-		System.out.println("next print");
+		System.out.println("next println");
 	}
 }
